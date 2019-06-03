14bb75531d4bc744ec9627434c0503c51b11b617
Jablan <joshuaablan@gmail.com>
2019-06-03 15:04:10 -0700

    create basic file structure

---
+++ app.js
@@ -0,0 +1,28 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("../project-priority/routes/index");
+const usersRouter = require("../project-priority/routes/users");
+
+const app = express();
+
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use((req, res, next)=>{
+  next(createError(404));
+});
+
+
+
+module.exports = app;
