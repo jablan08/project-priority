cb8acc01d4192b9b5825bde0308b71223285e957
Jablan <joshuaablan@gmail.com>
2019-06-09 15:33:26 -0700

    add styling to home-mid section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 const HomeContainer = styled.div`
     display:flex;
+    flex-direction: column;
+
 `
 const HomeTop = styled.section`
     display: flex;
@@ -62,7 +64,7 @@ const HomeTop = styled.section`
             text-align: center;
             align-items: center;
             box-sizing: border-box;
-            font-size: 2rem;
+            font-size: 1.5rem;
         }
         
     }
@@ -71,6 +73,75 @@ const HomeTop = styled.section`
         width: 400px;
         max-width: 90%;
         padding: 0 50px;
+    }
+
+`
+const HomeMid = styled.section`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    width: 100%;
+    border-radius: 5px;
+    box-sizing: border-box;
+    
+
+    .mid-container {
+        display: flex;
+        margin: 70px 0;
+        
+        max-width: 960px;
+        width: 100%;
+        justify-content: space-between;
+
+    }
+    .text-content {
+        max-width: 45%;
+        margin: 0 0 30px;
+        > ul {
+        /* list-style: none; */
+            > li {
+                list-style-type: disc;
+                margin: 0 0 .5rem 3.5rem;
+                font-size: 1.5rem;
+                line-height: 3rem;
+            }
+        }
+    }
+    .title {
+        color: #333;
+        max-width: 30rem;
+        font-size: 2.6rem;
+        line-height: 34px;
+        letter-spacing: .03em;
+        font-weight: 700;
+        @media (max-width: 50rem) {
+            margin: 0 0 1rem;
+            text-align: center;
+            align-items: center;
+            box-sizing: border-box;
+            font-size: 2.5rem;
+        }
+    }
+    .subtitle {
+        margin: 1.5rem 0 0;
+        max-width: 30rem;
+        color: #666;
+        font-size: 1.7rem;
+        line-height: 2.4rem;
+        @media (max-width: 50rem) {
+            margin: 0 0 1rem;
+            text-align: center;
+            align-items: center;
+            box-sizing: border-box;
+            font-size: 1.5rem;
+        }
+    }
+    .img-tag {
+            width: 45rem;
+            max-width: 100%;
+            align-self: flex-start;
     }
 `
 const Home = () => 
@@ -84,9 +155,33 @@ const Home = () =>
                 <div className="right-side">
                     <img className="right-side-img" src="/imgs/temp_pp.gif"/>
                 </div>
-                
             </header>
         </HomeTop>
+        <HomeMid>
+            <div className="mid-container">
+                <div className="text-content">
+                    <h2 className="title">
+                        Manage expectations across the board.
+                    </h2>
+                    <p className="subtitle">
+                         Not every feature should be built. Communicate to customers and teammates which features have been prioritized.
+                    </p>
+                    <ul>
+                        <li>
+                            Provide explanations for product decisions.
+                        </li>
+                        <li>
+                            Encourage collaboration on prioritized features.
+                        </li>
+                    </ul>
+                </div>
+                <div className="img-side">
+                    <img className="img-tag" src="/imgs/feature-manage-pp.png"></img>
+                </div>
+
+            </div>
+
+        </HomeMid>
     </HomeContainer>
 
 
